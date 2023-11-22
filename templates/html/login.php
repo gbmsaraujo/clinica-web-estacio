601b6d85c5fc93ed51c674456245a32d1d68dcb1
leandroferreira30 <96580968+leandroferreira30@users.noreply.github.com>
2023-11-22 15:34:09 -0300

    Página de login e NavBar das páginas

---
+++ templates/html/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+
+<body>
+    <main class="container">
+        <h2>Login</h2>
+        <form action="" method="get">
+            <div class="input-field">
+                <input type="text" name="usuario" id="usuario" placeholder="Nome de Usuário">
+                <div class="underline"></div>
+            </div>
+
+            <div class="input-field">
+                <input type="password" name="senha" id="senha" placeholder="Senha">
+                <div class="underline"></div>
+            </div>
+
+            <input type="submit" value="Entrar">
+            <a href="cadastro_usu.php">Clique aqui para se cadastrar!</a>
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ templates/html/quemsomos.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <title>Sobre</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+          Rede Health
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="quemsomos.php">Quem Somos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="servicos.php">Serviços</a>
+            </li>
+            <li class="nav-item" style="float: right;">
+              <a class="nav-link" href="login.php">Entrar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
