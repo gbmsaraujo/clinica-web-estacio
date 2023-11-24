de5aaa3ac92b04a82a8db0f69814f5048c2981ea
leandroferreira30 <96580968+leandroferreira30@users.noreply.github.com>
2023-11-24 12:54:02 -0300

    Pagina de serviços pronta, fodase

--- templates/html/servicos.php
+++ templates/html/servicos.php
@@ -46,46 +46,57 @@
   </header>
 
 
-  <main>
-    <h2>Exames realizados em nossa clínica</h2>
+  <section class="serv-sec1">
+    <h2 class="exames-realizados">Exames realizados em nossa clínica</h2>
 
-    <section>
+    <p>Seja bem-vindo à Clínica Médica Rede Health, onde priorizamos sua saúde e bem-estar através de serviços médicos de alta qualidade e tecnologia avançada. Nossa clínica oferece uma variedade de exames que visam proporcionar um diagnóstico preciso e uma abordagem personalizada para cada paciente. Confira abaixo os exames realizados por nossa clínica:</p>
 
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card">
-            <img src="../images/hemograma.jpg" class="card-img-top hemograma" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Hemograma Completo</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="../images/eletrocardiograma.jpg" class="card-img-top eletro" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Eletrocardiograma</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="../images/ressonancia magnetica.jpg" class="card-img-top ressonancia" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Ressonância Magnética</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+
+  </section>
+
+  <section class="serv-sec2">
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="../images/hemograma.jpg" class="card-img-top hemograma" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Hemograma Completo</h5>
+            <p class="card-text">O hemograma completo é um exame de sangue abrangente que fornece informações valiosas sobre diferentes componentes sanguíneos. Avaliamos as células vermelhas, células brancas e plaquetas, oferecendo uma visão detalhada da sua saúde sanguínea. Esse exame é fundamental para o diagnóstico de anemias, infecções e diversas condições hematológicas, possibilitando uma intervenção rápida e eficaz.</p>
           </div>
         </div>
       </div>
-    </section>
-  </main>
+      <div class="col">
+        <div class="card">
+          <img src="../images/eletrocardiograma.jpg" class="card-img-top eletro" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Eletrocardiograma</h5>
+            <p class="card-text">O eletrocardiograma (ECG) é um exame crucial para avaliar a atividade elétrica do coração. Este procedimento não invasivo registra a variação elétrica gerada pelo músculo cardíaco, permitindo-nos identificar eventuais irregularidades no ritmo cardíaco. O ECG é uma ferramenta essencial na prevenção e diagnóstico de problemas cardíacos.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../images/ressonancia magnetica.jpg" class="card-img-top ressonancia" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Ressonância Magnética</h5>
+            <p class="card-text">A ressonância magnética é uma técnica avançada de imagem que utiliza campos magnéticos e ondas de rádio para criar imagens detalhadas dos órgãos e estruturas internas do corpo. Este exame é fundamental para o diagnóstico de diversas condições, permitindo uma análise minuciosa de áreas como o cérebro, coluna vertebral, articulações e tecidos moles.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="serv-sec3">
+    <p>Agende sua consulta na Clínica Médica Rede Health e confie em nossa equipe para cuidar da sua saúde com dedicação, expertise e tecnologia avançada. Seu bem-estar é a nossa prioridade.</p>
+    <br>
+    <a href="login.php" class="btn btn-primary agendar" >Clique aqui para agendar!</a>
+  </section>
 
 
   <footer>
     <!-- place footer here -->
+    <p>&copy; 2023 Rede Health</p>
   </footer>
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
